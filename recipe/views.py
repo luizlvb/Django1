83180e5c08b1bc4fb3a04972984a7a56f70291c8
luizlvb <lfellipaovin45@gmail.com>
2024-05-27 11:20:12 -0300

    aa

--- recipe/views.py
+++ recipe/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipe/pages/home.html')
 
 def sobre(request):
     return HttpResponse('sobre')
