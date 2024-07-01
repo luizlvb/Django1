b72ee6d0ebdf5d61ce0b67e13ecdfb2609d2dbd9
luizlvb <lfellipaovin45@gmail.com>
2024-07-01 08:50:46 -0300

    sodade

--- recipe/models.py
+++ recipe/models.py
@@ -3,26 +3,32 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField((""), max_length=65)
+    name = models.CharField(max_length=65)
 
-class Recipes(models.Model):
-    title = models.CharField((""), max_length=65)
-    description = models.CharField((""), max_length=165)    
-    slug = models.SlugField((""))
-    preparation_time = models.IntegerField((""))
-    preparation_time_unit = models.CharField((""), max_length=65)
-    servings = models.IntegerField((""))
-    servings_unit = models.CharField((""), max_length=65)
-    preparation_steps = models.TextField((""))
+    def __str__(self) -> str:
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField( max_length=65)
+    description = models.CharField( max_length=165)    
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField( max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
-    created_at = models.DateTimeField((""), auto_now_add=True)
-    updated_at = models.DateField((""), auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField((""), upload_to='recipe/covers/%Y/%m/%d/', height_field=None, width_field=None, max_length=None)
+    cover = models.ImageField(upload_to='recipe/covers/%Y/%m/%d/', height_field=None, width_field=None, max_length=None)
     category = models.ForeignKey(   
-        Category, on_delete=models.SET_NULL, null=True, related_name='category'
+        Category, on_delete=models.SET_NULL, null=True, related_name='Category'
     )
     
     author = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, related_name='author'
+        User, on_delete=models.SET_NULL, null=True, related_name='Author'
     )
+    def __str__(self):
+        return self.title
+
